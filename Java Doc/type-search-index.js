9f27c051c53d1c4a981cdd7c94115c2d227a074b
Merleen25 <89622788+Merleen25@users.noreply.github.com>
2021-09-04 08:05:20 -0700

    JavaDoc
    no se habia generado javaDoc, ahora si esta incluido

---
+++ Java Doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Automovil"},{"p":"<Unnamed>","l":"Camion"},{"p":"<Unnamed>","l":"GestionParqueos"},{"p":"<Unnamed>","l":"Motocicleta"},{"p":"<Unnamed>","l":"TipoVehiculos"},{"p":"<Unnamed>","l":"Utils"},{"p":"<Unnamed>","l":"Vehiculo"}]
